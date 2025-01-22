d3aa8f7996a420c2a5ec5e35830ccbbc2491e0c9
Lombardi2003 <lombardidaniele2003@gmail.com>
2025-01-23 00:10:40 +0100

    Aggiornamento main

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-def main():
-    pass
+import os
+from create_dataset import *
 
+
+# Supponiamo che esista una cartella chiamata "Dataset"
+path = "Dataset"
+def download_dataset():
+    if os.path.isdir(path):
+        print(f"{path} è caricato.")
+    else:
+        print(f"Inizio download del dataset")
+        create()
+
+# Funzione main
+def main():
+    download_dataset()
+
+# Inizio programma
 if __name__ == '__main__':
     main()
