8157d0e2f92234ce4a0b1c864eae37abf6dfeb87
Lombardi2003 <lombardidaniele2003@gmail.com>
2025-01-23 15:10:52 +0100

    Aggiornamento file main

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+# Librerie di sistema
 import os
+import zipfile
+# Importazione di funzioni personali
 from create_dataset import *
-
 
 # Supponiamo che esista una cartella chiamata "Dataset"
 path = "Dataset"
 def download_dataset():
     if os.path.isdir(path):
         print(f"{path} è caricato.")
+    elif os.path.exists(path+'.zip'):
+        print(f"Estrazione del dataset...")
+        # Apri il file ZIP e estrai tutto il suo contenuto nella directory di destinazione
+        with zipfile.ZipFile(path+'.zip', 'r') as zip_ref:
+            zip_ref.extractall()
+        print (f"Dataset estratto")
     else:
         print(f"Inizio download del dataset")
         create()
