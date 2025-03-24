25ed28ca18709df113fc72eaee8c383f2e13a087
FrancescaPink <francescarosa2003@gmail.com>
2025-03-24 15:17:25 +0100

    Aggiunta funzione di creazione tabella

--- Postgres/Develop/db_creation.py
+++ Postgres/Develop/db_creation.py
@@ -13,10 +13,29 @@ def create_db(database):
         cur = conn.cursor()
         cur.execute(f"CREATE DATABASE {database.DB_NAME};")
         cur.close()
+        create_table(conn)  # Call create_table function after creating the database
         conn.close()
         print(f"Database {database.DB_NAME} creato con successo.")
     except Exception as e:
         print(f"Errore durante la creazione del database: {e}")
 
 def create_table(conn):
-    pass
+    if conn:
+        try:
+            with conn.cursor() as cursor:
+                create_table_query = """
+                CREATE TABLE IF NOT EXISTS dataset (
+                    id SERIAL PRIMARY KEY,
+                    title TEXT,
+                    release_year INTEGER,      
+                    genres TEXT,
+                    average_rating REAL,
+                    description TEXT,
+                    type TEXT
+                );
+                """
+                cursor.execute(create_table_query)
+                conn.commit()  # Ensure changes are committed to the database
+                print("Tabella 'dataset' creata con successo.")
+        except Exception as e:
+            print(f"Errore nella creazione della tabella: {e}")
