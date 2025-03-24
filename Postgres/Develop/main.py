3c0e247d4abb95e3dd3443713d7f2476f3efe46f
FrancescaPink <francescarosa2003@gmail.com>
2025-03-24 15:19:09 +0100

    Main aggiornato

--- Postgres/Develop/main.py
+++ Postgres/Develop/main.py
@@ -1,5 +1,5 @@
 from database_config import DatabaseConfig
-from db_creation import create_db 
+from db_creation import create_db, create_table
 import psycopg2
 import time
 
@@ -15,23 +15,26 @@ def db_connection(config):
                 host=config.IP_ADDRESS,
                 port=config.PORT_NUMBER
             )
-            print("Connection established successfully")
+            print("Connessione stabilita con successo.\n")
             return conn  # Connessione riuscita, restituisce connessione
         except Exception as e:
             print(f"Errore di connessione: {e}")
             time.sleep(config.RECONNECT_INTERVAL)  # Attendi 1 secondo prima di ritentare
 
-    print("Connessione non riuscita dopo 7 tentativi.")
+    print("Connessione non riuscita dopo 7 tentativi.\n")
     return None  # Restituisce None se non riesce a connettersi
 
 def main():
     database = DatabaseConfig()    # Create a new database configuration
     conn = db_connection(database) # Connect to the database
     if conn is None:  
-        print("Creazione del database in corso...")
+        print("Creazione del database in corso...\n")
         create_db(database) # Avvia lo script di creazione del DB
         conn = db_connection(database)  # Riprova a connettersi dopo la creazione
-        print("Database creato e connesso con successo.")
+        print("Database creato e connesso con successo.\n")
+    
+    # Creazione della tabella
+    create_table(conn)
     
     #  Close the connection
     if conn:
